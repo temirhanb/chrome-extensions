5b59897150f04c1c21edc5bd7eb4b3e16b88f66a
temirhanb <temirhan023@gmaul.com>
2023-10-31 10:03:19 +0500

    - add script

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,20 @@
-const App = ()=>{
+import React from 'react'
+import { createRoot } from "react-dom/client";
+
+const App: React.FC = () => {
 
   console.log('hello world')
-  return(
-    <h1>Hello world</h1>
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium at aut, commodi doloremque ea enim est
+        eum impedit labore laborum libero nemo nesciunt nobis optio possimus quam recusandae voluptates?</p>
+    </div>
   )
 }
+
+const container = document.createElement('div')
+document.body.appendChild(container)
+const root = createRoot(container)
+root.render(<App/>)
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,13 @@
+const path = require('path')
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlPlugin = require("html-webpack-plugin");
+
 module.exports = {
   mode: "development",
-  entry: "./src/index.tsx",
+  devtool: 'cheap-module-source-map',
+  entry: {
+    index: path.resolve("./src/index.tsx")
+  },
   module: {
     rules: [{
       use: "ts-loader",
@@ -8,10 +15,22 @@ module.exports = {
       exclude: /node-modules/
     }]
   },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {from: path.resolve('src/manifest.json'), to: path.resolve('dist')},
+      ],
+    }),
+    new HtmlPlugin({
+      title: "Webpack App",
+      filename: "index.html",
+      chunks: ['index']
+    }),
+  ],
   resolve: {
     extensions: ['.tsx', '.ts', '.js']
   },
   output: {
-    filename: "index.js"
+    filename: "[name].js"
   }
 }
